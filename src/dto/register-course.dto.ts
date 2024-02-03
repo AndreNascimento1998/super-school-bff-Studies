afc42c306c5528d49d43a54fd996d8bdd2f27d8e
Andre <andre.ncardoso@hotmail.com>
2024-02-03 20:43:09 -0300

    feat: dto for type/parsed data

---
+++ src/dto/register-course.dto.ts
@@ -0,0 +1,10 @@
+export class RegisterCourseDto {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  cep: string;
+  cpf: string;
+  dateBirth: string;
+  documentFile: string;
+}
