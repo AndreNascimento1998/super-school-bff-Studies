6b167d3b68136bc9b0bf30bfa25345cedf13d74a
Andre <andre.ncardoso@hotmail.com>
2024-02-03 17:25:40 -0300

    refactor: exporting modules individually

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get('NODE_PORT') || 3000;
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
