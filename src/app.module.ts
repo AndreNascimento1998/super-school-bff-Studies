614ad139739219896ce4ba03f40b3fc6bbe215c1
Andre <andre.ncardoso@hotmail.com>
2024-02-03 17:30:21 -0300

    refactor: exporting modules individually

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { _controllerModule } from './controller/_controller.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'process';
+import { ConfigModule } from '@nestjs/config';
+import { _entityModule, entityExports } from './entity/_entity.module';
+import { Modality } from './entity/modality';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    _controllerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number.parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: entityExports,
+      synchronize: true,
+    }),
+    _entityModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
