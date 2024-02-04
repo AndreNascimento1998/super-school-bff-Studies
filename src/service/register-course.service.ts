f2430fe509e1e8d99ea620f46a8f75b94e4b3f1a
Andre <andre.ncardoso@hotmail.com>
2024-02-03 23:39:58 -0300

    feat: verify if student is already registered

--- src/service/register-course.service.ts
+++ src/service/register-course.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterCourseDto } from '../dto/register-course.dto';
 import { RegisterCourseRepository } from '../repository/register-course.repository';
+import { StudentRepository } from '../repository/student.repository';
 
 @Injectable()
 export class RegisterCourseService {
   constructor(
     private readonly registerCourseRepository: RegisterCourseRepository,
+    private readonly studentRepository: StudentRepository,
   ) {}
 
   public async postRegister(registerCourse: RegisterCourseDto) {
+    let student: RegisterCourseDto;
+    student = await this.studentRepository.findOne(registerCourse.cpf);
+
+    if (!student) {
+      student = await this.studentRepository.register({
+        id: registerCourse.id,
+        name: registerCourse.name,
+        email: registerCourse.email,
+        phone: registerCourse.phone,
+        cep: registerCourse.cep,
+        cpf: registerCourse.cpf,
+        dateBirth: registerCourse.dateBirth,
+        documentFile: registerCourse.documentFile,
+      });
+    }
+
+    console.log(student);
     return await this.registerCourseRepository.createRegister(registerCourse);
   }
 }
