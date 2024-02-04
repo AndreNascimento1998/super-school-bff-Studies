409989da96e8bc4449d2ad43ccaa495097e05fe1
Andre <andre.ncardoso@hotmail.com>
2024-02-03 23:38:01 -0300

    feat: entity student

---
+++ src/entity/student.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('students')
+export class Student {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  cep: string;
+
+  @Column()
+  cpf: string;
+
+  @Column()
+  dateBirth: string;
+
+  @Column()
+  documentFile: string;
+}
