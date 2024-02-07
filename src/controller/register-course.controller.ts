6268809976d73f434d72037cd30c9f672638b525
Andre <andre.ncardoso@hotmail.com>
2024-02-07 19:13:04 -0300

    added: treating file

--- src/controller/register-course.controller.ts
+++ src/controller/register-course.controller.ts
@@ -1,13 +1,42 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { RegisterCourseDto } from '../dto/register-course.dto';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { diskStorage } from 'multer';
 import { RegisterCourseService } from '../service/register-course.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
 
 @Controller('register-course')
 export class RegisterCourseController {
   constructor(private readonly registerCourseService: RegisterCourseService) {}
 
   @Post()
-  public async register(@Body() registerData: RegisterCourseDto) {
-    return this.registerCourseService.postRegister(registerData);
+  @UseInterceptors(
+    FileInterceptor('payloadFile', {
+      storage: diskStorage({
+        destination: './src/storage/docs',
+        filename: (req, file, callback) => {
+          const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const fileExt = extname(file.originalname);
+          const filename = `${uniqueName}${fileExt}`;
+
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  public async register(
+    @UploadedFile() payloadFile: Express.Multer.File,
+    @Body() registerData,
+  ) {
+    console.log(registerData);
+    return this.registerCourseService.postRegister(
+      JSON.parse(registerData.payloadData),
+      payloadFile.path,
+    );
   }
 }
